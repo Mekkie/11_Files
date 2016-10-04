7f7cf17ae362afcc85341bdd35a0e001c5a00d52
Nicolai <nico8829@edu.eal.dk>
2016-10-04 11:13:39 +0200

    Add project files.

---
+++ 11_Files/FileStockRepository.cs
@@ -0,0 +1,82 @@
+﻿using System.IO;
+using System.Collections.Generic;
+
+namespace _11_Files
+{
+    public class FileStockRepository : IStockRepository, IFileRepository
+    {
+        private DirectoryInfo repositoryDir;
+        private long id = 0;
+
+        public FileStockRepository(DirectoryInfo repositoryDir)
+        {
+            this.repositoryDir = repositoryDir;
+        }
+
+        public long NextId()
+        {
+            id++;
+            return id;
+        }
+
+        public string StockFileName(long number)
+        {
+
+            return "stock" + number + ".txt"; 
+            
+        }
+
+        public string StockFileName(Stock s)
+        {
+            return "stock" + s.Id + ".txt";
+      
+        }
+
+        public void SaveStock(IAsset ias)
+        {
+
+            NextId();
+            Stock s = (Stock)ias;
+            s.Id = id;
+            FileInfo f = new FileInfo(repositoryDir + StockFileName(s));
+            StockIO sio = new StockIO();
+            sio.WriteStock(f, s);
+        }
+        public Stock LoadStock(long l)
+        {
+            StockIO sio = new StockIO();
+
+            foreach (FileInfo item in repositoryDir.GetFiles())
+            {
+                if (item.ToString() == "stock" + l + ".txt")
+                {
+                    return sio.ReadStock(new FileInfo(repositoryDir + item.ToString()));
+                }
+            }
+            throw new System.Exception("File not found");
+        }
+
+        public void Clear()
+        {
+            foreach (var item in repositoryDir.GetFiles())
+            {
+                item.Delete();
+            }
+        }
+
+        public List<IAsset> FindAllStocks()
+        {
+            List<IAsset> l = new List<IAsset>();
+            StockIO sio = new StockIO();
+
+            foreach (var item in repositoryDir.GetFiles())
+            {
+                l.Add(sio.ReadStock(new FileInfo(repositoryDir + item.ToString())));
+            }
+            
+            return l;
+        }
+
+        
+    }
+}
---
+++ 11_Files/IStockRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_Files
+{
+    public interface IStockRepository
+    {
+        long NextId();
+        void SaveStock(IAsset s);
+
+        Stock LoadStock(long l);
+
+        void Clear();
+
+        List<IAsset> FindAllStocks();
+    }
+}
